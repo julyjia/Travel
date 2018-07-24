410a1a3537f49bff850cfaf6616db632835d0f7a
hu jia <1063767759@qq.com>
2018-07-24 20:52:45 +0800

    icons

---
+++ src/pages/home/components/icons.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="icons"></div>
+</template>
+<script>
+export default {
+  name: 'HomeIcons'
+}
+</script>
+<style lang="stylus" scoped>
+  .icons
+    overflow: hidden
+    height: 0
+    padding-bottom: 50%
+    background-color: green
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -2,12 +2,15 @@
 <div>
 <home-header></home-header>
 <home-swiper></home-swiper>
+<home-icons></home-icons>
 </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/icons'
+
 export default {
   name: 'Home',
   components: {
