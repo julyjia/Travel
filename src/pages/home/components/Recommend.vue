98be555f2b53322ef1a2078ac011d3b6d4a2d5b4
hu jia <1063767759@qq.com>
2018-08-20 12:50:45 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li
+        class="item border-bottom"
+        v-for="item of recommendList"
+        :key="item.id"
+      >
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/ sightp0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
+        title: '上海迪士尼乐园',
+        desc: '上海迪士尼乐园'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/ sightp0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
+        title: '上海迪士尼乐园',
+        desc: '上海迪士尼乐园'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/ sightp0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
+        title: '上海迪士尼乐园',
+        desc: '上海迪士尼乐园'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+ .title
+   margin-top: .2rem
+   line-height: .4rem
+   background: #eee
+   text-indent: .2rem
+ .item
+   overflow: hidden
+   display: flex
+   height: 1.9rem
+   .item-img
+     width: 1.7rem
+     height: 1.7rem
+     padding: .1rem
+   .item-info
+     flex: 1
+     padding: .1rem
+     min-width: 0
+     .item-title
+       line-height: .54rem
+       font-size: .32rem
+       ellipsis()
+     .item-desc
+       line-height: .4rem
+       color: #ccc
+       ellipsis()
+     .item-button
+       line-height: .4rem
+       margin-top: .16rem
+       background: #ff9300
+       padding: 0 .2rem
+       border-radius: .06rem
+       color: #fff
+</style>
--- src/pages/home/components/icons.vue
+++ src/pages/home/components/icons.vue
@@ -23,7 +23,7 @@ export default {
     return {
       iconList: [{
         id: '0001',
-        imgUrl: 'http://img1.qunarzz.com/piao/    fusion/1803/95/f3dd6c383aeb3b02.png',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
         desc: '景点门票'
       }, {
         id: '0002',
@@ -85,33 +85,35 @@ export default {
   .icons >>> .swiper-container
     height: 0
     padding-bottom: 50%
-  .icon
-    position: relative
-    overflow: hidden
-    float: left
-    width: 25%
-    height: 0
-    padding-bottom: 25%
-    .icon-img
-      position: absolute
-      top: 0
-      left: 0
-      right: 0
-      bottom: .44rem
-      box-sizing: border-box
-      padding: .1rem
-      .icon-img-content
-        display: block
-        margin: 0 auto
-        height: 100%
-    .icon-desc
-      position: absolute
-      left: 0
-      right: 0
-      bottom: 0
-      height: .44rem
-      line-height: .44rem
-      text-align: center
-      color: $darkTextColor
-      ellipsis()
+  .icons
+    margin-top: .1rem
+    .icon
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding: .1rem
+        .icon-img-content
+          display: block
+          margin: 0 auto
+          height: 100%
+      .icon-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        text-align: center
+        color: $darkTextColor
+        ellipsis()
 </style>
